2cb041921da4ef6e963ada18e64d631298b7f703
nikororo <nikoro52@gmail.com>
2020-08-16 19:31:35 +0700

    config

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAhCJ4H-egV4uoaIjiy4U4mjaiP59gj2OU",
+    authDomain: "task3back.firebaseapp.com",
+    databaseURL: "https://task3back.firebaseio.com",
+    projectId: "task3back",
+    storageBucket: "task3back.appspot.com",
+    messagingSenderId: "211899032039",
+    appId: "1:211899032039:web:0d11c448d4de1170c41d42",
+    measurementId: "G-BZBPV3STNH"
+  };
